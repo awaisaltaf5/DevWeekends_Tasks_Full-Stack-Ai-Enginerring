bd097ca316689eeea1a7d9090d5f3b3abdfb895f
Muhammad Awais Altaf <168270210+awaisaltaf5@users.noreply.github.com>
2026-07-11 08:20:29 -0400

    Initial project setup with Tailwind, Router, and Context providers

---
+++ tour-travel-app/src/App.jsx
@@ -0,0 +1,33 @@
+
+import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './contexts/ThemeContext'
+import { AuthProvider } from './contexts/AuthContext'
+import { SearchProvider } from './contexts/SearchContext'
+import MainLayout from './components/templates/MainLayout'
+import HomePage from './pages/HomePage'
+import DestinationsPage from './pages/DestinationsPage'
+import PackagesPage from './pages/PackagesPage'
+import AboutPage from './pages/AboutPage'
+import SearchResultsPage from './pages/SearchResultsPage'
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <SearchProvider>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/destinations" element={<DestinationsPage />} />
+              <Route path="/packages" element={<PackagesPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/search" element={<SearchResultsPage />} />
+            </Route>
+          </Routes>
+        </SearchProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  )
+}
+
+export default App
